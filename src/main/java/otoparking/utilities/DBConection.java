bd8f71c48bc7eca4dca0c6e96bda5c48e5460208
Bazzi <dinhkhoa288@gmail.com>
2026-01-21 04:53:27 -0500

    Update DBConection.java

--- src/main/java/otoparking/utilities/DBConection.java
+++ src/main/java/otoparking/utilities/DBConection.java
@@ -5,11 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConection {
-
-    private static final String connectionString = "jdbc:mysql://100.101.143.118:3306/OtoParking?useSSL=false&serverTimezone=UTC";
-
-    private static final String _USER = "wholock";
-    private static final String _PASSWORD = "221004";
+    private static final String connectionString = 
+        "jdbc:mysql://<YOUR_SERVER>:3306/OtoParking?useSSL=false&serverTimezone=UTC";
+    private static final String _USER = "<YOUR_USER>";
+    private static final String _PASSWORD = "<YOUR_PASSWORD>";
 
     public static Connection GetConnection() throws SQLException {
         return DriverManager.getConnection(connectionString, _USER, _PASSWORD);
